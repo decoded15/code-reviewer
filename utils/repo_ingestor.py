30f1bf8d5e9c6c49b072c5158cc2698560f67c74
Dibyansh Mohanty <mohantydibyansh77@gmail.com>
2026-06-06 20:24:43 +0530

    add functionality to read Python files and index repository with code chunking

--- utils/repo_ingestor.py
+++ utils/repo_ingestor.py
@@ -1,5 +1,6 @@
 import os
-
+from utils.code_chunker import chunk_python_code
+from embeddings.chroma_manager import store_chunks
 
 def get_python_files(repo_path):
 
@@ -32,3 +33,29 @@ def get_python_files(repo_path):
                 python_files.append(full_path)
 
     return python_files
+
+def read_file(file_path):
+
+    with open(
+        file_path,
+        "r",
+        encoding="utf-8"
+    ) as file:
+
+        return file.read()
+
+def index_repository(repo_path):
+
+    python_files = get_python_files(repo_path)
+
+    for file_path in python_files:
+
+        code = read_file(file_path)
+
+        chunks = chunk_python_code(code)
+
+        for chunk in chunks:
+
+            chunk["file_path"] = file_path
+
+        store_chunks(chunks)
